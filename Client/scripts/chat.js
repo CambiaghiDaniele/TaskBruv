57d79cc7f44f8efc18e3af745608b0c29ca8e54b
ChiodaPietro <104900072+ChiodaPietro@users.noreply.github.com>
2024-01-23 13:42:18 +0100

    Add files via upload

---
+++ Client/scripts/chat.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const btn=document.getElementById("addchat");
+    const chatlist=document.getElementById("chatlist");
+  
+})
+ function addChat(){
+    const h=document.createElement("h1");
+    h.innerHTML="mutanbo";
+    chatlist.appendChild(h)
+    console.log("hsdhfoahsid")
+}
