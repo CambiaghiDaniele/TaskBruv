87888f764a1a85a82d3e4d5bd9c2062630e78a84
Scarsella Kay Luca <scarsella.kay@osdbsesto>
2024-01-29 11:45:10 +0100

    boh

--- backend/send.php
+++ backend/send.php
@@ -1,5 +1,5 @@
 <?php
-
+mail($_POST["email"],"verifica il tuo accaunt TaskBruv","clicca questo link per verificare il tuo accaunt TaskBruv : file:///X:/Users/scarsella.kay/Desktop/TaskBruv/frontend/structure/login.html?",)
 
 
 ?>
