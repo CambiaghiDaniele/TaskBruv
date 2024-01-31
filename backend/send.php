dfbc80b2572803cc1ea559680d3c6f7973417270
Scarsella Kay Luca <scarsella.kay@osdbsesto>
2024-01-31 13:41:21 +0100

    agginta la verifica dell'eamil nella registrazione

--- backend/send.php
+++ backend/send.php
@@ -1,9 +1,15 @@
 <?php
+session_start();
+echo("ciao siamo fuori all'if");
+echo($_SESSION["randomPassword"]."siamo nel post </br>");
+echo($_GET["randomPassword"]."siamo nel get </br>");
 if($_GET["randomPassword"] === null){
-    $_POST["randomPassword"] = rand(100,100000);
-    mail($_POST["email"],"verifica il tuo accaunt TaskBruv","clicca questo link per verificare il tuo accaunt TaskBruv : file:///X:/Users/scarsella.kay/Desktop/TaskBruv/backend/send.php?randomPassword=".$_POST["randomPassword"]);
-}else if ($_GET["randomPassword"] == $_POST["randomPassword"]){
+    echo("ciao siamo dentro all'if");
+    $_SESSION["randomPassword"] = rand(100,100000);
+    mail($_POST["email"],"verifica eil tuo accaunt TaskBruv","clicca questo link per verificare il tuo accaunt TaskBruv : http://scarsella.kay.tave.osdb.it/TaskBruv/backend/send.php?randomPassword=".$_SESSION["randomPassword"]);
+}else if ($_GET["randomPassword"] == $_SESSION["randomPassword"]){
     echo("l'email è verificata");
+    session_destroy();
 }
 
 ?>
