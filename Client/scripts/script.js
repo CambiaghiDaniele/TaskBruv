3834923f16de6147ab29ff68e4089cf1e7cb8a30
ChiodaPietro <104900072+ChiodaPietro@users.noreply.github.com>
2024-02-01 22:55:01 +0100

    Delete Client directory

--- Client/scripts/script.js
+++
@@ -1,8 +0,0 @@
-/* design : https://www.uplabs.com/posts/messaging-app-2db4a257-7f1d-4d1c-970d-4cf6527247ff by Anastasia Marinicheva */
-document.addEventListener("DOMContentLoaded", function(){
-    const discussions = Array.from(document.querySelectorAll(".chat"));
-    console.log(discussions);
-   discussions.map((chat)=>chat.addEventListener("click",()=>{
-    console.log("clicked");
-   }))
-});
