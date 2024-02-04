b94c5b4211703f6666a65868904cde2103af84f8
kay scarsella <scarsella.kay@studenti.salesianisesto.it>
2024-02-04 21:04:29 +0100

    carico i cambiamenti

--- backend/send.php
+++ backend/send.php
@@ -1,15 +1,62 @@
 <?php
 session_start();
-echo("ciao siamo fuori all'if");
-echo($_SESSION["randomPassword"]."siamo nel post </br>");
-echo($_GET["randomPassword"]."siamo nel get </br>");
-if($_GET["randomPassword"] === null){
-    echo("ciao siamo dentro all'if");
-    $_SESSION["randomPassword"] = rand(100,100000);
-    mail($_POST["email"],"verifica eil tuo accaunt TaskBruv","clicca questo link per verificare il tuo accaunt TaskBruv : http://scarsella.kay.tave.osdb.it/TaskBruv/backend/send.php?randomPassword=".$_SESSION["randomPassword"]);
-}else if ($_GET["randomPassword"] == $_SESSION["randomPassword"]){
-    echo("l'email è verificata");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+
+$mail = new PHPMailer(TRUE);
+$_SESSION["UTENTE"][] = $_POST["UTENTE"];
+var_dump($_POST);
+    if($_GET["randomPassword"] === null){
+        try{
+            $mail->setFrom('kayscarsella@gmail.com', 'TaskBruv');
+            $mail->addAddress($_POST["email"], 'Destinatario');
+            $mail->Subject = 'verifica eil tuo accaunt TaskBruv';
+            $_SESSION["randomPassword"] = rand(100,100000);
+            $mail->Body = "clicca questo link per verificare il tuo accaunt TaskBruv : http://localhost/TaskBruv/backend/send.php?randomPassword=55497?randomPassword=".$_SESSION["randomPassword"];
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = TRUE;
+            $mail->Username = 'kayscarsella@gmail.com';
+            $mail->Password = 'wkvq lpsc ksfc pgqe';
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 587;
+            $mail->send();
+        }
+        catch (Exception $e)
+        {
+            echo $e->errorMessage();
+        }
+        catch (\Exception $e)
+        {
+            echo $e->getMessage();
+        }
+    }else if ($_GET["randomPassword"] == $_SESSION["randomPassword"]){
+        echo("<h1>hai finalmente confermato la tua email</h1>");
+        concectTODB();
+        session_destroy();
+    }
+
+?>
+<?php
+function concectTODB(){
+    $username = "root";
+    $password = "";
+    $database = "TaskBruv";
+    $utente = $_SESSION["UTENTE"];
+    $mysql = new mysqli("localhost", $username, $password, $database);
+    $utenti = $mysql->query("SELECT * FROM taskbruv.utente");
+    while($row = $utenti->fetch_assoc()) {
+       if(($row["email"] === $utente["email"])||($row["nome"] === $utente["username"])){
+            echo("nome utente o eamil gia utilizzati");
+       }else{
+        $sql = "INSERT INTO c248_taskbruv.utente (email, nome, password) VALUES ('" . $utente['email'] . "', '" . $utente['username'] . "', '" . $utente['password'] . "')";
+        $mysql->query($sql);
+       }
+    }
     session_destroy();
 }
-
 ?>
